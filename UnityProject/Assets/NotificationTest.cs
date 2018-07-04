4c479375325eb2e01b2768179ded3c74a16b6d79
Mehdi Mehri <Mehdi.2m712@gmail.com>
2018-07-04 18:49:24 +0430

    Create new demo scene and add notification test controller
    create new demo scene for test all type of notifications

--- UnityProject/Assets/NotificationTest.cs
+++ UnityProject/Assets/NotificationTest.cs
@@ -1,43 +1,107 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class NotificationTest : MonoBehaviour
 {
-    void Awake()
+	public Toggle sound;
+	public Toggle light;
+	public Toggle vibration;
+	public Text status;
+	public Text delayText;
+	public InputField title;
+	public InputField description;
+	public InputField action1;
+	public InputField action2;
+	public Slider delay;
+	public Dropdown type;
+
+	void Awake()
     {
         LocalNotification.ClearNotifications();
+		status.text = "";
     }
 
-    public void OneTime()
-    {
-        LocalNotification.SendNotification(1, 5000, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
-    }
+	#region UI events ======================================
+	public void notificationSend(){
 
-    public void OneTimeBigIcon()
-    {
-        LocalNotification.SendNotification(1, 5000, "Title", "Long message text with big icon", new Color32(0xff, 0x44, 0x44, 255), true, true, true, "app_icon");
-    }
+		string title = this.title.text;
+		string text = this.description.text;
+		string action1 = this.action1.text;
+		string action2 = this.action2.text;
+		long delay =	Convert.ToInt64(this.delay.value.ToString ()) * 1000;
+		string type = this.type.captionText.text;
 
-    public void OneTimeWithActions()
-    {
-        LocalNotification.Action action1 = new LocalNotification.Action("background", "In Background", this);
-        action1.Foreground = false;
-        LocalNotification.Action action2 = new LocalNotification.Action("foreground", "In Foreground", this);
-        LocalNotification.SendNotification(1, 5000, "Title", "Long message text with actions", new Color32(0xff, 0x44, 0x44, 255), true, true, true, null, "boing", "default", action1, action2);
-    }
+		if (title == "")
+			title = "test title";
 
-    public void Repeating()
-    {
-        LocalNotification.SendRepeatingNotification(1, 5000, 60000, "Title", "Long message text", new Color32(0xff, 0x44, 0x44, 255));
-    }
+		if (text == "")
+			text = "test text";
 
-    public void Stop()
-    {
-        LocalNotification.CancelNotification(1);
-    }
+		if (action1 == "")
+			action1 = "action 1";
 
-    public void OnAction(string identifier)
-    {
-        Debug.Log("Got action " + identifier);
-    }
+		if (action2 == "")
+			action2 = "action 2";
+		
+		switch (type){
+
+		case "Small Icon Notification":
+			status.text = "small icon notification set";
+			LocalNotification.SendNotification(1, delay, title, text, new Color32(0xff, 0x44, 0x44, 255), sound.isOn, light.isOn, vibration.isOn);
+			break;
+
+		case "Big Icon Notification":
+			status.text = "Big icon notification set";
+			LocalNotification.SendNotification(1, delay, title, text, new Color32(0xff, 0x44, 0x44, 255), sound.isOn, light.isOn, vibration.isOn, "app_icon");
+			break;
+
+		case "Action Notification":
+			status.text = "Action notification set";
+			LocalNotification.Action actionBtn1 = new LocalNotification.Action ("action1", action1, this);
+			actionBtn1.Foreground = false;
+			LocalNotification.Action actionBtn2 = new LocalNotification.Action ("action2", action2, this);
+			actionBtn2.Foreground = true;
+			LocalNotification.SendNotification(1, delay, title, text, new Color32(0xff, 0x44, 0x44, 255), sound.isOn, light.isOn, vibration.isOn, null, "boing", "default", actionBtn1, actionBtn2);
+			break;
+
+		case "Repeat":
+			status.text = "notification repeat";
+			LocalNotification.SendRepeatingNotification(1, delay, 5000, title, text, new Color32(0xff, 0x44, 0x44, 255), sound.isOn, light.isOn, vibration.isOn);
+			break;
+
+		case "Stop":
+			status.text = "notification stop";
+			LocalNotification.CancelNotification(1);
+			break;
+
+		default:
+				
+			break;
+
+		}
+
+	}
+
+	public void OnDelayChange(){
+		delayText.text = delay.value.ToString ();
+	}
+
+	public void exitGame(){
+		Application.Quit ();
+	}
+
+	public void onTypeChange(){
+		//string type = this.type.captionText.text;
+		if (type.value != 2){	//type == "Action Notification") {
+			action1.interactable = false;
+			action2.interactable = false;
+		} else {
+			action1.interactable = true;
+			action2.interactable = true;
+		}
+	}
+
+	#endregion
 }
